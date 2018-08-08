03243deec4d3af8649c74a6843bc90b71154b418
netanel <gal.netanel.mobile@gmail.com>
2018-08-08 18:05:48 +0300

    initial draft

--- basicCoding.py
+++ basicCoding.py
@@ -19,9 +19,27 @@ myString1 = "hello1"
 myString2 = "hello2"
 myDic = {'firstName':'Gal', 'lastName':'Netanel'}
 
-printMe(myString1, myString2, name="xxx", value='yyy', )
-printMe(myString1, myString2, myDic )
-printMe(**myDic )
+#printMe(myString1, myString2, name="xxx", value='yyy', )
+#printMe(myString1, myString2, myDic )
+#printMe(**myDic )
+
+class Animal:
+    name = None
+    def __init__(self, name):
+        self.name = name;
+
+    def printInfo(self):
+        print("name = " + self.name)
+
+
+
+class Horse(Animal):
+    type = "horse"
+
+a = Animal("dummy")
+b = Horse("second dummy");
+a.printInfo()
+b.printInfo()
 
 
 
