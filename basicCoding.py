137d93ef29447c913ecc94b7813b878e0d6e39e8
netanel <gal.netanel.mobile@gmail.com>
2018-08-08 17:57:58 +0300

    initial draft

--- basicCoding.py
+++ basicCoding.py
@@ -17,5 +17,11 @@ def printMe(*arg, **kwargs):
 
 myString1 = "hello1"
 myString2 = "hello2"
+myDic = {'firstName':'Gal', 'lastName':'Netanel'}
+
 printMe(myString1, myString2, name="xxx", value='yyy', )
+printMe(myString1, myString2, myDic )
+printMe(**myDic )
+
+
 
