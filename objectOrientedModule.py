1747bee4c205b10a6acf0e8706b00fefa0267a79
netanel <gal.netanel.mobile@gmail.com>
2018-08-09 08:09:14 +0300

    initial draft

--- objectOrientedModule.py
+++ objectOrientedModule.py
@@ -3,7 +3,7 @@ def dummy():
 
 class Animal():
     name = None
-    animalType = None
+    animalType = "Empty type"
     def __init__(self, name):
         self.name=name
 
@@ -11,13 +11,23 @@ class Animal():
         print("My name is " + self.name)
         print("My type is " + self.animalType)
 
+    def printMe2(self):
+        print("printme2 of animal class")
+
+
 class Dog(Animal):
     def __init__(self, name):
         Animal.__init__(self, name)
         self.animalType = 'Dog'
 
+    def printMe2(self):
+        print("printme2 of animal Dog class")
 
 def runme():
-    myDog = Dog("Lucky");
-    myDog.printMe()
+    myDog = Dog("Lucky2");
+    myAnimal = Animal("Lucky1");
 
+    myDog.printMe()
+    myDog.printMe2()
+    myAnimal.printMe()
+    myAnimal.printMe2()
