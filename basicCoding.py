448dca4fe177c469e321945a6f5f75015eebdb47
netanel <gal.netanel.mobile@gmail.com>
2018-08-12 07:39:18 +0300

    add functions and organized main python file

--- basicCoding.py
+++ basicCoding.py
@@ -2,7 +2,7 @@ import mymodule
 import listFunctions
 import objectOrientedModule
 import pyFunctions
-import sqlite3
+import dbcode
 
 #mymodule.greeting("Jonathan")
 #listFunctions.listfunc()
@@ -14,34 +14,9 @@ import sqlite3
 
 
 
-def dbcode():
-    connection = sqlite3.connect("db/test.db");
-    # this will create dababase if file not exists
-    cursor = connection.cursor()
-
-    sql = '''CREATE TABLE IF NOT EXISTS Person
-                (PID INTEGER PRIMARY KEY AUTOINCREMENT,
-                 NAME VARCHAR(100), 
-                 HEIGHT INT)'''
-
-    # create the table if needed
-    cursor.execute(sql)
 
 
-    sql = "INSERT INTO Person (NAME, HEIGHT) VALUES ('Jayanam', 174)"
-    # insert new person into data
-    cursor.execute(sql)
-    # commit for writing to file
-    connection.commit()
-    sql = "SELECT * FROM Person"
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-    for row in rows:
-        print (row)
-    connection.close()
-
-
-dbcode()
+dbcode.dbcode()
 
 #a = Animal("dummy")
 #b = Horse("second dummy");
---
+++ dbcode.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+def dbcode():
+    connection = sqlite3.connect("db/test.db");
+    # this will create dababase if file not exists
+    cursor = connection.cursor()
+
+    sql = '''CREATE TABLE IF NOT EXISTS Person
+                (PID INTEGER PRIMARY KEY AUTOINCREMENT,
+                 NAME VARCHAR(100), 
+                 HEIGHT INT)'''
+
+    # create the table if needed
+    cursor.execute(sql)
+
+
+    sql1 = "INSERT INTO Person (NAME, HEIGHT) VALUES ('Jayanam', 175)"
+    sql2 = "INSERT INTO Person (NAME, HEIGHT) VALUES ('Jayanam', 176)"
+    sql3 = "INSERT INTO Person (NAME, HEIGHT) VALUES ('Jayanam', 178)"
+
+
+    # insert new person into data
+    cursor.execute(sql1)
+    cursor.execute(sql2)
+    cursor.execute(sql3)
+
+    # commit for writing to file
+    connection.commit()
+    sql = "SELECT * FROM Person"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    for row in rows:
+        print (row)
+    connection.close()
+
